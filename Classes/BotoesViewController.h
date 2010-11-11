f634ab50ee498c88e187a4a0b576690d61e242f1
bruno soares <brunosoares@bruno-soaress-MacBook.local>
2010-11-11 19:29:04 -0300

    projeto do mare de agilidade que utiliza actions em botoes do iphone

---
+++ Classes/BotoesViewController.h
@@ -0,0 +1,23 @@
+//
+//  BotoesViewController.h
+//  Botoes
+//
+//  Created by bruno soares on 11/11/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BotoesViewController : UIViewController {
+
+	UILabel * label;
+	
+}
+
+@property(retain, nonatomic) IBOutlet UILabel * label;
+
+
+- (IBAction) buttonPressed: (id) sender;
+
+@end
+
